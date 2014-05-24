be7870fe5f1de00fd5d8db4faaeca1eba5fdb8c6
Juan Huerta <juan.huerta@gmail.com>
2014-05-24 12:04:25 +0200

    Getting every single result for each race into separate elements. Each element, contains both the race details and the information for that particular athelete. Ready to save each of this entries into the table

--- scraper.py
+++ scraper.py
@@ -1,25 +1,17 @@
-DATABASE_NAME = 'data.sqlite'
-
 import os
-os.environ['SCRAPERWIKI_DATABASE_NAME'] = DATABASE_NAME
-
 import scraperwiki
 import lxml.html
 from pprint import pprint
 
-result_schema = {
-        'rank': None,
-        'bib': None,
-        'fis_code': None,
-        'name': None,
-        'year_of_birth': None,
-        'nation': None,
-        'run_1': None,
-        'run_2': None,
-        'total_time': None,
-        'diff': None,
-        'fis_points': None,
-}
+DATABASE_NAME = 'data.sqlite'
+os.environ['SCRAPERWIKI_DATABASE_NAME'] = DATABASE_NAME
+
+FIS_URL = "http://data.fis-ski.com/alpine-skiing/results.html"
+for link, raceinfo in race_link_results(FIS_URL):
+    raceResults = get_race_results(link)
+    for result in raceResults:
+        complete_athlete_result_line = dict(result.items() + raceinfo.items())
+        print complete_athlete_result_line
 
 # generator of FIS race links
 def race_link_results(url):
@@ -36,25 +28,83 @@ def race_link_results(url):
             for event_row in event_rows:
                 last_cell = event_row.cssselect("td:last-child")[0]
                 for race_links in last_cell.iterlinks():
+                    tdSelected = event_row.cssselect("td")
                     extra = {
-                        'date': get_cell_value(event_row.cssselect("td")[1], "span a"),
-                        'place': get_cell_value(event_row.cssselect("td")[2], "span a"),
-                        'country': get_cell_value(event_row.cssselect("td")[3], "a span"),
-                        'codex': get_cell_value(event_row.cssselect("td")[4], "a"),
-                        'discipline': get_cell_value(event_row.cssselect("td")[5], "a"),
+                        'date': get_cell_value(tdSelected[1], "span a"),
+                        'place': get_cell_value(tdSelected[2], "span a"),
+                        'country': get_cell_value(tdSelected[3], "a span"),
+                        'codex': get_cell_value(tdSelected[4], "a"),
+                        'discipline': get_cell_value(tdSelected[5], "a"),
                     }
                     yield (race_links[2], extra)
 
 def get_cell_value(element, css):
     return element.cssselect(css)[0].text_content()
 
-FIS_URL = "http://data.fis-ski.com/alpine-skiing/results.html"
-for link, raceinfo in race_link_results(FIS_URL):
+def save_text_element_print(element):
+    print element.text_content().encode('ascii', 'ignore')
+
+def save_print(element):
+    print element.encode('ascii', 'ignore')
+
+def get_race_results(url):
+    raceResults = []
+    html = scraperwiki.scrape(url)
+    root = lxml.html.fromstring(html)
+    result_rows = root.cssselect("div.bloc-tab")[1].cssselect("table.footable tbody tr")
+    for result_row in result_rows:
+        if row_contains_information(result_row):
+            athlete_data = extract_data_for_this_athlete(result_row)
+            raceResults.append(athlete_data)
+    return raceResults
+
+def row_contains_information(row):
+    NUMBER_OF_COLUMNS_WITH_INFO = 11
+    #A row contains information if it has 11 columns. 
+    #Some rows in the table don't have any information, stating only that the rider didn't start
+    tdElementsInRow = row.cssselect("td")
+    return len(tdElementsInRow) == NUMBER_OF_COLUMNS_WITH_INFO
+
+def extract_data_for_this_athlete(row):
+    return {
+        'Rank': get_plain_element_from_column_number(row,0),
+        'Bib': get_plain_element_from_column_number(row,1),
+        'FISCode': get_plain_element_from_column_number(row,2),
+        'Name': get_plain_element_from_column_number(row,3),
+        'Year': get_plain_element_from_column_number(row,4),
+        'Nation': get_plain_element_from_column_number(row,5),
+        'Run_1': get_plain_element_from_column_number(row,6),
+        'Run_2': get_plain_element_from_column_number(row,7),
+        'Total_Time': get_plain_element_from_column_number(row,8),
+        'Time_Diff': get_plain_element_from_column_number(row,9),
+        'FIS_Points': get_plain_element_from_column_number(row,10),
+    }
+
+def get_plain_element_from_column_number(element,colNumber):
+    return element.cssselect("td")[colNumber].text_content()        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #save_print(complete_athlete_result_line)
+    #for raceResult in raceResults:
+        #print raceResult
+    #print raceinfo
+
     # html = scraperwiki.scrape(link)
     # root = lxml.html.fromstring(html)
     # result_table = root.cssselect("table.fisfootable")[0]
-    print link
-    print raceinfo['date']
+    # print raceinfo['date']
 
 # # Write out to the sqlite database using scraperwiki library
 # data = {"name": "susan", "occupation": "software developer"}
